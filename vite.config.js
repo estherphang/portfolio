b0264612e220d5897efdd1ab8c22f50dc5a3a606
estney <esther.pxy@gmail.com>
2024-05-23 14:57:32 +0800

    homepage url

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         404: resolve(__dirname, "public/404.html"),
       },
-      outDir: "dist",
     },
   },
   base: "/portfolio",
