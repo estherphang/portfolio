95e73f6718912dc96436553730d61776481ef861
estney <esther.pxy@gmail.com>
2024-09-03 11:29:52 +0800

    edit links to css

--- src/Components/Homepage/Main.jsx
+++ src/Components/Homepage/Main.jsx
@@ -1,5 +1,5 @@
 import RecentWork from "./RecentWork";
-import "./homepage.css";
+import "/src/Components/Homepage/homepage.css";
 import { yellowBtn } from "../StyleLibrary/ClassName";
 import { Link } from "react-router-dom";
 
